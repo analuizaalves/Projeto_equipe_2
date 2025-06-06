44338d4ecf98763cc925cdc9a7d9a9dab4611994
Vicky-pixeluna <vitoriatereza@aluno.ifce.edu.br>
2025-06-06 10:47:20 -0300

    uma mensagem

--- sitebiotec/app/page.tsx
+++ sitebiotec/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
       <div><p>alteração:Ana Lu</p></div>
       <div><p>Outra alteração: Sophia</p></div>
       <div><p>Alteração: Mirellinha</p></div>
+      <div><p>Alteração: It's Viih Bitch!</p></div>
     </>
   );
 }
