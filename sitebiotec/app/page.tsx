f44038a22fa04ef0d6e8327e7d9e95625bd9cfd7
Sooh2025 <mirella.vieira07@aluno.ifce.edu.br>
2025-06-06 10:43:30 -0300

    Alteração

--- sitebiotec/app/page.tsx
+++ sitebiotec/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <>
       <div><p>alteração:Ana Lu</p></div>
       <div><p>Outra alteração: Sophia</p></div>
+      <div><p>Alteração: Mirellinha</p></div>
     </>
   );
 }
